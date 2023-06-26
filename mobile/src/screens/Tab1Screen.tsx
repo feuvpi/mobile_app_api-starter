37fd6e2f77d625eb8a9399e4204c710b967de7ab
feuvpi <fredvpg@gmail.com>
2023-06-25 23:31:07 -0300

    adding tab1 screen

---
+++ mobile/src/screens/Tab1Screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export function Tab1Screen() {
+  return (
+    <View className="flex-1 bg-background px-8 pt-16" style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text className="text-white">Tab</Text>
+    </View>
+  );
+}
